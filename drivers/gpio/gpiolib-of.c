02bb1a522d81211e6c0d339906871af598b73a28
Robert Jarzmik <robert.jarzmik@free.fr>
2015-02-07 22:08:32 +0100

    Revert "gpiolib: of: Correct error handling in of_get_named_gpiod_flags"
    This reverts commit 7b8792bbdffdff3abda704f89c6a45ea97afdc62.

--- drivers/gpio/gpiolib-of.c
+++ drivers/gpio/gpiolib-of.c
@@ -45,14 +45,8 @@ static int of_gpiochip_find_and_xlate(struct gpio_chip *gc, void *data)
 		return false;
 
 	ret = gc->of_xlate(gc, &gg_data->gpiospec, gg_data->flags);
-	if (ret < 0) {
-		/* We've found the gpio chip, but the translation failed.
-		 * Return true to stop looking and return the translation
-		 * error via out_gpio
-		 */
-		gg_data->out_gpio = ERR_PTR(ret);
-		return true;
-	 }
+	if (ret < 0)
+		return false;
 
 	gg_data->out_gpio = gpiochip_get_desc(gc, ret);
 	return true;
